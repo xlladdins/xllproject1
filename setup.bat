def701bfff81463f63f0e9ba19d2ab4fbcb42b72
Keith A. Lewis <kal@kalx.net>
2021-04-02 10:23:06 -0400

    Delete setup.bat

--- setup.bat
+++
@@ -1,8 +0,0 @@
-REM setup.bat - set up git and user settings
-@echo off
-git init
-git submodule add https://github.com/xlladdins/xll.git
-copy xll\test\test.vcxproj.user xllproject1.vcxproj.user
-echo Add xll\xll.vcxproj to your solution.
-echo Build and debug by pressing F5.
-set /p dummy=Hit Enter to continue
